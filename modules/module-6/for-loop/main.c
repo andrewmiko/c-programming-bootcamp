91f712cb359d66084053189343287052ad7388f7
Andrew Miko <andrejs.miko@wearea2.com>
2024-07-05 22:08:45 +0200

    Added for loops

---
+++ modules/module-6/for-loop/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    // For loops are useful when you know the number of iterations
+    for (int i = 0; i < 10; i++)
+    {
+        printf("i = %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+}
