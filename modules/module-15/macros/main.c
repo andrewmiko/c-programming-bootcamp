eea5ab8f4343344b02345cb92f1b655a57dacc9f
Andrew Miko <andrejs.miko@wearea2.com>
2024-07-30 18:59:55 +0200

    Add macros

---
+++ modules/module-15/macros/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+int main(int argc, char const *argv[])
+{
+    /**
+     * Macros are a way to define constants or functions that can be used in your code.
+     * They are defined using the #define directive.
+     *
+     * Example:
+     * #define PI 3.14159
+     *
+     * Macros can also be used to define functions.
+     *
+     * Example:
+     * #define SQUARE(x) x * x
+     */
+
+    printf("The value of PI is: %f\n", PI);
+
+    return EXIT_SUCCESS;
+}
